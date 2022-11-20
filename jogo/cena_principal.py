fa0d390aa29730d5c562cfcd1cc7a2e125d3ab0a
robertolt <rl.beto.lorenzoni@gmail.com>
2022-11-20 11:18:24 -0300

    começando a implementar personagens e etc

---
+++ jogo/cena_principal.py
@@ -0,0 +1,79 @@
+import sys
+from typing import Tuple
+import pygame as pg
+# from bola import Bola
+
+from config_jogo import ConfigJogo
+from config_personagem import Personagem
+from estado_jogo import EstadoJogo
+
+
+class CenaPrincipal:
+    def __init__(self, tela):
+        self.tela = tela
+
+        py = ConfigJogo.ALTURA_TELA // 2
+        px_esq = ConfigJogo.LARGURA_TELA//2 - 10
+        px_dir = ConfigJogo.LARGURA_TELA//2 + 10
+
+        self.personagem1 = Personagem(posicao=(px_esq, py))
+        self.personagem2 = Personagem(posicao=(px_dir, py))
+        # self.bola = Bola()
+        self.estado = EstadoJogo()
+
+        self.encerrada = False
+
+    def rodar(self):
+        while not self.encerrada:
+            self.tratamento_eventos()
+            self.atualiza_estado()
+            self.desenha()
+
+    def tratamento_eventos(self):
+        pg.event.get()
+
+        # evento de saida
+        if pg.key.get_pressed()[pg.K_ESCAPE]:
+            sys.exit(0)
+
+        # personagem 1
+        if pg.key.get_pressed()[pg.K_w]:
+            self.personagem1.mover_para_cima()
+        elif pg.key.get_pressed()[pg.K_s]:
+            self.personagem1.mover_para_baixo()
+        elif pg.key.get_pressed()[pg.K_a]:
+            self.personagem1.mover_para_esquerda()
+        elif pg.key.get_pressed()[pg.K_a]:
+            self.personagem1.mover_para_direita()
+        else:
+            self.personagem1.parar()
+
+        # personagem 2
+        if pg.key.get_pressed()[pg.K_i]:
+            self.personagem2.mover_para_cima()
+        elif pg.key.get_pressed()[pg.K_k]:
+            self.personagem2.mover_para_baixo()
+        elif pg.key.get_pressed()[pg.K_j]:
+            self.personagem2.mover_para_direita()
+        elif pg.key.get_pressed()[pg.K_l]:
+            self.personagem2.mover_para_esquerda()
+        else:
+            self.personagem2.parar()
+
+    def atualiza_estado(self):
+        self.personagem1.atualizar_posicao_x()
+        self.personagem1.atualizar_posicao_y()
+        self.personagem2.atualizar_posicao_x()
+        self.personagem2.atualizar_posicao_y()
+        #  self.bola.atualizar_posicao()
+
+        if self.estado.jogo_terminou():
+            self.encerrada = True
+
+    def desenha(self):
+        self.tela.fill((255, 255, 255))
+        self.personagem1.desenha(self.tela)
+        self.personagem2.desenha(self.tela)
+        # self.bola.desenha(self.tela)
+        self.estado.desenha(self.tela)
+        pg.display.flip()
--- jogo/config_personagem.py
+++ jogo/config_personagem.py
@@ -7,51 +7,59 @@ class Personagem:
     VIDA = 20
     DANO = 2
     VELOCIDADE = 0.2
-    TIPO_ATAQUE = ATAQUE
 
-        def __init__(self, posicao):
-            self.posicao = posicao
-            self.velocidade = 0
+    # TIPO_ATAQUE = ATAQUE
 
-        def mover_para_cima(self):
-            self.velocidade = -ConfigJogo.VELOCIDADE_PERSONAGEM
+    def __init__(self, posicao):
+        self.posicao = posicao
+        self.velocidade = 0
 
-        def mover_para_baixo(self):
-            self.velocidade = ConfigJogo.VELOCIDADE_PERSONAGEM
+    def mover_para_cima(self):
+        self.velocidade = -ConfigJogo.VELOCIDADE_PERSONAGEM
 
-        def parar(self):
-            self.velocidade = 0
+    def mover_para_baixo(self):
+        self.velocidade = ConfigJogo.VELOCIDADE_PERSONAGEM
 
-        def atualizar_posicao(self):
-            x, y = self.posicao
-            novo_y = y + self.velocidade
-            novo_x = x + self.velocidade
+    def mover_para_esquerda(self):
+        self.velocidade = ConfigJogo.VELOCIDADE_PERSONAGEM
 
-            if (novo_y >= 0) and \
-                    ((novo_y + ConfigJogo.ALTURA_PERSONAGEM) <= ConfigJogo.ALTURA_TELA):
-                self.posicao = (x, novo_y)
+    def mover_para_direita(self):
+        self.velocidade = - ConfigJogo.VELOCIDADE_PERSONAGEM
 
-            if (novo_x >= 0) and \
-                    ((novo_x + ConfigJogo.ALTURA_PERSONAGEM) <= ConfigJogo.LARGURA_TELA):
-                self.posicao = (novo_x, y)
+    def parar(self):
+        self.velocidade = 0
 
-        def desenha(self, tela):
-            x = self.posicao[0]
-            y = self.posicao[1]
-            l = ConfigJogo.LARGURA_BARRA
-            a = ConfigJogo.ALTURA_BARRA
-            pygame.draw.rect(
-                tela,
-                ConfigJogo.COR_BARRA,
-                pygame.rect.Rect(x, y, l, a)
-            )
+    def atualizar_posicao_y(self):
+        x, y = self.posicao
+        novo_y = y + self.velocidade
 
-        def rect(self) -> Tuple[float, float, float, float]:
-            """ retorna os dados da barra como os retangulos sao representados
-                no pygame, i.e., como uma tupla do tipo (px, py, largura, altura).
-            """
-            return self.posicao + (ConfigJogo.LARGURA_BARRA, ConfigJogo.ALTURA_BARRA)
+        if (novo_y >= 0) and \
+                ((novo_y + ConfigJogo.ALTURA_PERSONAGEM) <= ConfigJogo.ALTURA_TELA):
+            self.posicao = (x, novo_y)
 
+    def atualizar_posicao_x(self):
+        x, y = self.posicao
+        novo_x = x + self.velocidade
+        if (novo_x >= 0) and \
+                ((novo_x + ConfigJogo.LARGURA_PERSONAGEM) <= ConfigJogo.LARGURA_TELA):
+            self.posicao = (novo_x, y)
+
+    def desenha(self, tela):
+        x = self.posicao[0]
+        y = self.posicao[1]
+        l = ConfigJogo.LARGURA_PERSONAGEM
+        a = ConfigJogo.ALTURA_PERSONAGEM
+        pygame.draw.rect(
+            tela,
+            ConfigJogo.COR_PERSONAGEM,
+            pygame.rect.Rect(x, y, l, a)
+        )
+
+    def rect(self) -> Tuple[float, float, float, float]:
+        """ retorna os dados da barra como os retangulos sao representados
+            no pygame, i.e., como uma tupla do tipo (px, py, largura, altura).
+        """
+        return self.posicao + (ConfigJogo.LARGURA_PERSONAGEM, ConfigJogo.ALTURA_PERSONAGEM)
 
 # class Mago(Personagem):
 #     def __init__(self, tipo_ataque):
